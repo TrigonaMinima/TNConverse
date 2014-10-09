118d0aed825285e806a7fac61c2937aea36e3fa5
nkmittal4994 <nkmittal4994@gmail.com>
2014-10-08 17:54:03 -0700

    proper chat bewtween client and server and stops when client enters 'end'

--- Python/Unicast/client.py
+++ Python/Unicast/client.py
@@ -10,8 +10,12 @@ port = 12345
 # connect to the server on local computer
 s.connect(('127.0.0.1', port))
 while True:
-# receive data from the server
-    print s.recv(1024)
-    s.send("bye!!")
+    # receive data from the server
+    print "Server sent :%s" % s.recv(1024)
+    x = raw_input('enter ur msg :')
+    #sending message to the server
+    s.send(x)
+    if x == 'end':
+        break
 # close the connection
 s.close()
--- Python/Unicast/server.py
+++ Python/Unicast/server.py
@@ -15,18 +15,22 @@ print "socket binded to %s" %(port)
 
 # put the socket into listening mode
 s.listen(5)     
-print "socket is listening"            
+print "socket is listening" 
+
+# Establish connection with client.
+c, addr = s.accept()     
+print 'Got connection from', addr
 
 # a forever loop until we interrupt it or 
-# an error occurs
 while True:
-   # Establish connection with client.
-   c, addr = s.accept()     
-   print 'Got connection from', addr
-
-   # send a thank you message to the client. 
-   c.send('Thank you for connecting')
-   # Close the connection with the client
-   print c.recv(1024)
-   
+    # Establish connection with client.
+    x = raw_input('enter ur msg :')
+    # send a message to the client. 
+    c.send(x)
+    # print the received msg from client side
+    y = c.recv(1024)
+    print "Client sent :%s" % y 
+    if y == 'end':
+        break
+# Close the connection with the client  
 c.close()
